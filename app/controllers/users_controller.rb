a32003e2908a17baaac93a7635e91be2ec128f5b
Geoff Wild <geoffwild11@gmail.com>
2015-05-01 14:41:40 +0000

    User sign up now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # handle save case
+      flash[:success] = "Welcome to the Wild Philosophy Blog"
+      redirect_to @user
     else
       render 'new'
     end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  
+  test "should not create invalid user" do
+    get signup_path
+    
+    # Verify that attempting to create an invalid user doesn't work
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: " ",
+                               email: "ho@u",
+                               password: "he",
+                               paswword_confirmation: "nmatch" }
+    end
+    assert_template 'users/new'
+  end
+  
+  test "should create a valid user" do
+    get signup_path
+    
+    # Verify that sending valid user info to form works
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Test User",
+                                            email: "test@example.com",
+                                            password: "hellothere",
+                                            password_confirmation: "hellothere" }
+    end
+    assert_template 'users/show'
+  end
+  
+  # Need a test for not creating duplicate user?
+end
